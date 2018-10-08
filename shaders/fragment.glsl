eccf97514df079aea0eb30fca62dfc13379096b6
archmoj <mojtaba@plot.ly>
2018-10-08 17:48:55 -0400

    using glslify for out-of-bound function

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,14 +1,11 @@
 precision mediump float;
+
+#pragma glslify: outOfRange = require(./reversed-scenes-out-of-range.glsl)
+
 uniform vec3 clipBounds[2];
 uniform float opacity;
 varying vec3 fragPosition;
 varying vec4 fragColor;
-
-bool outOfRange(float a, float b, float p) {
-  if (p > max(a, b)) return true;
-  if (p < min(a, b)) return true;
-  return false;
-}
 
 void main() {
   if ((outOfRange(clipBounds[0].x, clipBounds[1].x, fragPosition.x)) ||
